0cd7ffe5496ddc82b96f6b1a8e6d0afc06802de2
William <concejo@CB-20583917.CONCEJOBTA.LOC>
2024-11-25 17:40:56 -0500

    formulario decameron

---
+++ src/DecameronApp.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Reservation from './components/Reservation'
+import Availability from './components/Availability'
+
+const DecameronApp = () => {
+  return (
+    <div>
+
+<Reservation />
+<Availability/>
+
+
+
+    </div>
+  )
+
+
+
+
+}
+
+export default DecameronApp
